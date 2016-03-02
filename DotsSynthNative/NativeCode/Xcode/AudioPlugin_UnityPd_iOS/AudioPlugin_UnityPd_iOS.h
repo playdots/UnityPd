fbfddc05e963806c06050d2a9d60606342960a25
Eddie Cameron <eddie@weplaydots.com>
2016-03-02 15:06:12 -0500

    Build for iPhone - edited Unity include `AudioPluginInterface.h` to get rid of wrong iOS redefinition of UInt32/SInt32 on 32- bit devices

---
+++ DotsSynthNative/NativeCode/Xcode/AudioPlugin_UnityPd_iOS/AudioPlugin_UnityPd_iOS.h
@@ -0,0 +1,13 @@
+//
+//  AudioPlugin_UnityPd_iOS.h
+//  AudioPlugin_UnityPd_iOS
+//
+//  Created by Eddie Cameron on 3/2/16.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AudioPlugin_UnityPd_iOS : NSObject
+
+@end
