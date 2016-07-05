0f8dd9dc83b376c1bc723e7e397322f5611d1388
Eddie Cameron <eddie@weplaydots.com>
2016-07-05 14:29:19 -0400

    - add locks to unityPD - use UnityPd_ native functions to wrap pd calls

--- UnityPd/Bindings/UnityPD.cs
+++ UnityPd/Bindings/UnityPD.cs
@@ -48,14 +48,14 @@ public class UnityPD : MonoBehaviour {
     private static Dictionary<int, IntPtr> _openPatches = new Dictionary<int, IntPtr>();
 
 #if UNITY_EDITOR_WIN
-    private static void libpd_EnableAudio() { }
+    private static void UnityPd_EnableAudio() { }
 #else
     #if UNITY_IPHONE
     [DllImport( "__Internal" )]
     #else
     [DllImport("AudioPlugin_UnityPd")]
     #endif
-    private static extern void libpd_EnableAudio();
+    private static extern void UnityPd_EnableAudio();
 #endif
 
     public static void Init() {
@@ -66,7 +66,7 @@ public class UnityPD : MonoBehaviour {
             }
         }
 
-        libpd_EnableAudio();
+        UnityPd_EnableAudio();
 
 #if UNITY_ANDROID && !UNITY_EDITOR
         CopyPdResourcesToPersistentPath();
@@ -77,7 +77,7 @@ public class UnityPD : MonoBehaviour {
 
     public static void Deinit() {
         foreach ( var patchPtr in _openPatches.Values )
-            libpd_ClosePatch( patchPtr );
+            UnityPd_ClosePatch( patchPtr );
         _openPatches.Clear();
 
         ClearSearchPath();
@@ -88,21 +88,21 @@ public class UnityPD : MonoBehaviour {
 
 
 #if UNITY_EDITOR_WIN
-    private static void libpd_clear_search_path() { }
+    private static void UnityPd_ClearSearchPath() { }
 #else
     #if UNITY_IPHONE
     [DllImport( "__Internal" )]
     #else
     [DllImport("AudioPlugin_UnityPd")]
     #endif
-    private static extern void libpd_clear_search_path();
+    private static extern void UnityPd_ClearSearchPath();
 #endif
 
     /// <summary>
     /// clears the search path for pd externals
     /// </summary>
     public static void ClearSearchPath() {
-        libpd_clear_search_path();
+        UnityPd_ClearSearchPath();
     }
 
     public static void ClearAddedSearchPaths() {
@@ -113,14 +113,14 @@ public class UnityPD : MonoBehaviour {
 
 
 #if UNITY_EDITOR_WIN
-    private static void libpd_add_to_search_path( string path ) { }
+    private static void UnityPd_AddToSearchPath( string path ) { }
 #else
     #if UNITY_IPHONE
     [DllImport( "__Internal" )]
     #else
     [DllImport("AudioPlugin_UnityPd")]
     #endif
-    private static extern void libpd_add_to_search_path( string path );
+    private static extern void UnityPd_AddToSearchPath( string path );
 #endif
 
     /// <summary>
@@ -128,29 +128,29 @@ public class UnityPD : MonoBehaviour {
     /// </summary>
     /// <param name="sym">directory to add</param>
     public static void AddToSearchPath( string sym ) {
-        libpd_add_to_search_path( sym );
+        UnityPd_AddToSearchPath( sym );
     }
 
 #if UNITY_EDITOR_WIN
-    private static int libpd_GetDollarZero( IntPtr intPtr ) { return -1; }
+    private static int UnityPd_GetDollarZero( IntPtr intPtr ) { return -1; }
 #else
     #if UNITY_IPHONE
     [DllImport( "__Internal" )]
     #else
     [DllImport("AudioPlugin_UnityPd")]
     #endif
-    private static extern int libpd_GetDollarZero( IntPtr intPtr );
+    private static extern int UnityPd_GetDollarZero( IntPtr intPtr );
 #endif
 
 #if UNITY_EDITOR_WIN
-    private static IntPtr libpd_OpenPatch( string patchName, string patchDir ) { return default( IntPtr ); }
+    private static IntPtr UnityPd_OpenPatch( string patchName, string patchDir ) { return default( IntPtr ); }
 #else
     #if UNITY_IPHONE
     [DllImport( "__Internal" )]
     #else
     [DllImport("AudioPlugin_UnityPd")]
     #endif
-    private static extern IntPtr libpd_OpenPatch( string patchName, string patchDir );
+    private static extern IntPtr UnityPd_OpenPatch( string patchName, string patchDir );
 #endif
 
     public static int OpenPatch( string patchName ) {
@@ -164,13 +164,13 @@ public class UnityPD : MonoBehaviour {
         }
 
         Debug.Log( string.Format( "[#UnityPd] Opening patch {0} in {1}", patchName, patchDir ) );
-        var ptr = libpd_OpenPatch( patchName, patchDir );
+        var ptr = UnityPd_OpenPatch( patchName, patchDir );
 
         if ( ptr == IntPtr.Zero ) {
             throw new IOException( "unable to open patch " + patchName );
         }
 
-        int dollarZero = libpd_GetDollarZero( ptr );
+        int dollarZero = UnityPd_GetDollarZero( ptr );
         _openPatches.Add( dollarZero, ptr );
 
         return dollarZero;
@@ -178,14 +178,14 @@ public class UnityPD : MonoBehaviour {
     }
 
 #if UNITY_EDITOR_WIN
-    private static void libpd_ClosePatch( IntPtr patchPtr ) {}
+    private static void UnityPd_ClosePatch( IntPtr patchPtr ) {}
 #else
     #if UNITY_IPHONE
     [DllImport( "__Internal" )]
     #else
     [DllImport("AudioPlugin_UnityPd")]
     #endif
-    private static extern void libpd_ClosePatch( IntPtr patchPtr );
+    private static extern void UnityPd_ClosePatch( IntPtr patchPtr );
 #endif
 
     public static void ClosePatch( int patchHandle ) {
@@ -194,7 +194,7 @@ public class UnityPD : MonoBehaviour {
 #else
         IntPtr patchPtr;
         if ( _openPatches.TryGetValue( patchHandle, out patchPtr ) ) {
-            libpd_ClosePatch( patchPtr );
+            UnityPd_ClosePatch( patchPtr );
             _openPatches.Remove( patchHandle );
         }
         else
@@ -222,17 +222,45 @@ public class UnityPD : MonoBehaviour {
 
     #region Messages
 #if UNITY_EDITOR_WIN
-    private static void libpd_float( string receiver, float message ) { }
+    private static void UnityPd_SendFloat( string receiver, float message ) { }
 #else
     #if UNITY_IPHONE
     [DllImport( "__Internal" )]
     #else
     [DllImport("AudioPlugin_UnityPd")]
     #endif
-    private static extern void libpd_float( string receiver, float message );
+    private static extern void UnityPd_SendFloat( string receiver, float message );
 #endif
     public static void SendFloat( string receiver, float message ) {
-        libpd_float( receiver, message );
+        UnityPd_SendFloat( receiver, message );
+    }
+
+#if UNITY_EDITOR_WIN
+    private static void UnityPd_SendBang( string receiver ) { }
+#else
+#if UNITY_IPHONE
+    [DllImport( "__Internal" )]
+#else
+    [DllImport("AudioPlugin_UnityPd")]
+#endif
+    private static extern void UnityPd_SendBang( string receiver );
+#endif
+    public static void SendBang( string receiver ) {
+        UnityPd_SendBang( receiver );
+    }
+
+#if UNITY_EDITOR_WIN
+    private static void UnityPd_SendSymbol( string receiver, string message ) { }
+#else
+#if UNITY_IPHONE
+    [DllImport( "__Internal" )]
+#else
+    [DllImport("AudioPlugin_UnityPd")]
+#endif
+    private static extern void UnityPd_SendSymbol( string receiver, string message );
+#endif
+    public static void SendSymbol( string receiver, string message ) {
+        UnityPd_SendSymbol( receiver, message );
     }
     #endregion
 }
